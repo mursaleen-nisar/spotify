9e9833314f095309fab6111031cc85238bb7d93d
Suhaib <mursaleennisar980@gmail.com>
2024-01-02 11:56:26 +0530

    added baseURL for fetch

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,4 @@
+const baseURL = "https://spotifybymursaleen.freewebhostmost.com";
 let currentSong = new Audio();
 let songs;
 let currFolder;
@@ -21,7 +22,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`/${folder}/`);
+    let a = await fetch(`${baseURL}/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -73,7 +74,7 @@ const playMusic = (track, pause=false) => {
 
 
 async function displayAlbums() {
-    let a = await fetch(`/songs/`);
+    let a = await fetch(`${baseURL}/songs/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -85,7 +86,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs/")) {
             let folder = e.href.split("/").slice(-1)[0];
             // Get the metadata of the folder
-            let a = await fetch(`/songs/${folder}/info.json`);
+            let a = await fetch(`${baseURL}/songs/${folder}/info.json`);
             let response = await a.json();
             document.querySelector(".card-container").innerHTML += `<div data-folder="${folder}" class="card">
             <div class="play">
